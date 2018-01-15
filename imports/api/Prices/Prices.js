284c069fe0ac2bbd18c7f3a170f7addf42491f0a
MichelFloyd <michel.floyd@cloaklabs.com>
2018-01-16 00:12:52 +0100

    Polling now upserts currency prices into Prices collection

---
+++ imports/api/Prices/Prices.js
@@ -0,0 +1,40 @@
+/* eslint-disable consistent-return */
+
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+const Prices = new Mongo.Collection('Prices');
+
+Prices.allow({
+  insert: () => false,
+  update: () => false,
+  remove: () => false,
+});
+
+Prices.deny({
+  insert: () => true,
+  update: () => true,
+  remove: () => true,
+});
+
+Prices.schema = new SimpleSchema({
+  id: { type: String, label: 'The coinmarketbase id' },
+  name: { type: String, label: 'The name' },
+  symbol: { type: String, label: 'The symbol' },
+  rank: { type: Number, label: 'Rank by market cap' },
+  price_usd: { type: Number, label: 'Price in USD' },
+  price_btc: { type: Number, label: 'Price in BTC' },
+  volume_usd_24h: { type: Number, label: '24h volume in USD', optional: true },
+  market_cap_usd: { type: Number, label: 'total market cap' },
+  available_supply: { type: Number, label: 'Available supply' },
+  total_supply: { type: Number, label: 'Total supply' },
+  max_supply: { type: Number, label: 'Maximum supply', optional: true },
+  percent_change_1h: { type: Number, label: 'Percent change 1h', optional: true },
+  percent_change_24h: { type: Number, label: 'Percent change 24h', optional: true },
+  percent_change_7d: { type: Number, label: 'Percent change 7d', optional: true },
+  last_updated: { type: Date, label: 'Last Updated' },
+});
+
+Prices.attachSchema(Prices.schema);
+
+export default Prices;
--- imports/modules/server/cron-poll-coinmarketcap.js
+++ imports/modules/server/cron-poll-coinmarketcap.js
@@ -1,23 +1,39 @@
 import { HTTP } from 'meteor/http';
+import Prices from '../../api/Prices/Prices';
 
 const api = 'https://api.coinmarketcap.com/v1/ticker/?limit=';
-const limit = 2000;
+const limit = 2000; // the top 2000 currencies. As of this writing the API returns 1442
 const url = `${api}${limit}`;
 
 SyncedCron.add({
-  name: 'Poll coinmarketcap API every 30 seconds',
+  name: 'Poll coinmarketcap API every minute',
   schedule(parser) {
-    return parser.text('every 30 seconds');
+    return parser.text('every 1 minute');
   },
   job() {
     HTTP.get(url, (err, response) => {
       if (!err) {
-        response.data.forEach((el, i) => {
-          if (i < 10) console.log(`Symbol: ${el.symbol} price (USD): ${el.price_usd}`);
+        response.data.forEach((el) => {
+          if (el.market_cap_usd) {
+            const price = { id: el.id, name: el.name, symbol: el.symbol };
+            price.rank = parseInt(el.rank);
+            price.price_usd = parseFloat(el.price_usd);
+            price.price_btc = parseFloat(el.price_btc);
+            if (el['24h_volume_usd']) price.volume_usd_24h = parseFloat(el['24h_volume_usd']);
+            price.market_cap_usd = parseFloat(el.market_cap_usd);
+            price.available_supply = parseFloat(el.available_supply);
+            price.total_supply = parseFloat(el.total_supply);
+            if (el.max_supply) price.max_supply = parseFloat(el.max_supply);
+            if (el.percent_change_1h) price.percent_change_1h = parseFloat(el.percent_change_1h);
+            if (el.percent_change_24h) price.percent_change_24h = parseFloat(el.percent_change_24h);
+            if (el.percent_change_7d) price.percent_change_7d = parseFloat(el.percent_change_7d);
+            price.last_updated = new Date(parseFloat(el.last_updated * 1000));
+            Prices.upsert({ id: price.id, last_updated: price.last_updated }, { $set: price });
+          }
         });
+        console.log(Prices.find().count());
       } else console.log(err);
     });
-    return true;
   },
 });
 
