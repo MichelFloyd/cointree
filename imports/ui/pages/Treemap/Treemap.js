e4e37bf9653ba32b5977c06d285351f82b50603b
MichelFloyd <michel.floyd@cloaklabs.com>
2018-01-28 19:10:36 +0100

    Basic UI for controlling the treemap (but not wired yet)

--- imports/ui/pages/Treemap/Treemap.js
+++ imports/ui/pages/Treemap/Treemap.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ButtonToolbar, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import d3 from 'd3';
@@ -9,38 +9,63 @@ import TreeMap from '../../components/D3/Treemap/Treemap';
 
 import './index.scss';
 
-const Treemap = ({ loading, data, colorAccessor, colors, totalCap, vol24h }) => (!loading ? (
-  <div className="Treemap">
-    <div className="TreemapControls">
-      <ButtonToolbar>
-        Total Market Cap: <b>${(totalCap / 1e9).toFixed(0)}B</b> Volume 24h: <b>${(vol24h / 1e9).toFixed(0)}B</b>
-        &nbsp;Cell size:&nbsp;
-        <ToggleButtonGroup type="radio" name="sizeOptions" defaultValue={1}>
-          <ToggleButton value={1}>Market Cap</ToggleButton>
-          <ToggleButton value={2}>Volume 24h</ToggleButton>
-        </ToggleButtonGroup>
-        &nbsp;Cell color:&nbsp;
-        <ToggleButtonGroup type="radio" name="colorOptions" defaultValue={1}>
-          <ToggleButton value={1}>1h change</ToggleButton>
-          <ToggleButton value={2}>24h change</ToggleButton>
-          <ToggleButton value={2}>7d change</ToggleButton>
-        </ToggleButtonGroup>
-      </ButtonToolbar>
-    </div>
-    <div className="Treemap">
-      <TreeMap
-        height={500}
-        width={window.innerWidth - 80}
-        data={data}
-        valueUnit="USD"
-        textColor="#ccc"
-        fontSize={14}
-        colors={colors}
-        colorAccessor={colorAccessor}
-      />
-    </div>
-  </div>
-) : <Loading />);
+class Treemap extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.setSize = this.setSize.bind(this);
+    this.setColor = this.setColor.bind(this);
+
+    this.state = {
+      radioSize: 0,
+      radioColor: 0,
+    };
+
+    this.sizeMenu = ['Market Cap', 'Volume 24'];
+    this.colorMenu = ['1h Change', '24h Change', '7d Change']
+  }
+
+  setSize(ekey) {
+    this.setState({ radioSize: ekey - 1 });
+  }
+
+  setColor(ekey) {
+    this.setState({ radioColor: ekey - 1 });
+  }
+
+  render() {
+    const { loading, data, colorAccessor, colors, totalCap, vol24h } = this.props;
+    return (!loading ? (
+      <div className="Treemap">
+        <div className="TreemapControls">
+            Total Market Cap: <b>${(totalCap / 1e9).toFixed(0)}B</b> Volume 24h: <b>${(vol24h / 1e9).toFixed(0)}B</b>
+            &nbsp;Cell size:&nbsp;
+            <DropdownButton id="sizeOptions" title={this.sizeMenu[this.state.radioSize]} value={this.state.radioSize} onSelect={this.setSize}>
+              <MenuItem eventKey={1} padding={0}>{this.sizeMenu[0]}</MenuItem>
+              <MenuItem eventKey={2} padding={0}>{this.sizeMenu[1]}</MenuItem>
+            </DropdownButton>
+            &nbsp;Cell color:&nbsp;
+            <DropdownButton id="colorOptions" title={this.colorMenu[this.state.radioColor]} value={this.state.radioColor} onSelect={this.setColor}>
+              <MenuItem eventKey={1}>{this.colorMenu[0]}</MenuItem>
+              <MenuItem eventKey={2}>{this.colorMenu[1]}</MenuItem>
+              <MenuItem eventKey={3}>{this.colorMenu[2]}</MenuItem>
+            </DropdownButton>
+        </div>
+        <div className="Treemap">
+          <TreeMap
+            height={500}
+            width={window.innerWidth - 80}
+            data={data}
+            valueUnit="USD"
+            textColor="#ccc"
+            fontSize={14}
+            colors={colors}
+            colorAccessor={colorAccessor}
+          />
+        </div>
+      </div>
+    ) : <Loading />);
+  }
+}
 
 Treemap.propTypes = {
   loading: PropTypes.bool.isRequired,
@@ -63,9 +88,9 @@ export default withTracker(() => {
       data.push({ label: price.symbol, value: price.volume_usd_24h, link: `/prices/${price.symbol}`, value2: colorAccessor(price) });
       if (colorAccessor(price) < min) min = colorAccessor(price);
       if (colorAccessor(price) > max) max = colorAccessor(price);
-      totalCap += price.market_cap_usd;
-      vol24h += price.volume_usd_24h;
     }
+    totalCap += price.market_cap_usd;
+    vol24h += price.volume_usd_24h;
   });
 
   const colors = (v) => {
