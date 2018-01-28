6213ac8b4629587461e41390004b765220da1289
MichelFloyd <michel.floyd@cloaklabs.com>
2018-01-28 22:16:22 +0100

    Changing the treemap really isn't working, colors don't get updated.

--- imports/ui/pages/Treemap/Treemap.js
+++ imports/ui/pages/Treemap/Treemap.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { DropdownButton, MenuItem } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
-import { ReactiveVar } from 'meteor/reactive-var';
 import d3 from 'd3';
 import Loading from '../../components/Loading/Loading';
 import LatestPrices from '../../../api/LatestPrices/LatestPrices';
@@ -10,42 +9,89 @@ import TreeMap from '../../components/D3/Treemap/Treemap';
 
 import './index.scss';
 
-const sizeSelectorOptions = ['market_cap_usd', 'volume_usd_24h'];
-const colorSelectorOptions = ['percent_change_1h', 'percent_change_24h', 'percent_change_7d'];
-const sizeSelector = new ReactiveVar(sizeSelectorOptions[0]);
-const colorSelector = new ReactiveVar(colorSelectorOptions[0]);
-
 class Treemap extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.setSize = this.setSize.bind(this);
     this.setColor = this.setColor.bind(this);
 
+    this.sizeMenu = ['Market Cap', 'Volume 24'];
+    this.colorMenu = ['1h Change', '24h Change', '7d Change']
+    this.sizeSelectorOptions = ['market_cap_usd', 'volume_usd_24h'];
+    this.colorSelectorOptions = ['percent_change_1h', 'percent_change_24h', 'percent_change_7d'];
+  
     this.state = {
       radioSize: 0,
       radioColor: 0,
+      data: [],
+      colors: () => {},
+      totalCap: 0,
+      vol24h: 0,
     };
+  }
 
-    this.sizeMenu = ['Market Cap', 'Volume 24'];
-    this.colorMenu = ['1h Change', '24h Change', '7d Change']
+  componentWillMount() {
+    const { data, colors, totalCap, vol24h } = this.getData();
+    this.setState({ data, colors, totalCap, vol24h });  
   }
 
   setSize(ekey) {
     this.setState({ radioSize: ekey - 1 });
-    sizeSelector.set(sizeSelectorOptions[ekey - 1]);
+    //this.setState({ data: []});
+    const { data, colors, totalCap, vol24h } = this.getData();
+    this.setState({ data, colors, totalCap, vol24h });
   }
 
   setColor(ekey) {
     this.setState({ radioColor: ekey - 1 });
-    colorSelector.set(colorSelectorOptions[ekey - 1]);
+    //this.setState({ data: []});
+    const { data, colors, totalCap, vol24h } = this.getData();
+    this.setState({ data, colors, totalCap, vol24h });
+  }
+
+  getData() {
+    const { prices } = this.props;
+    const sizeSelector = this.sizeSelectorOptions[this.state.radioSize];
+    const colorSelector = this.colorSelectorOptions[this.state.radioColor];
+
+    console.log(`Updating with size: ${sizeSelector} color: ${colorSelector}`);
+    const data = [];
+    const colorAccessor = p => p[colorSelector];
+  
+    let min = prices && prices[0] && colorAccessor(prices[0]);
+    let max = min;
+    let totalCap = 0;
+    let vol24h = 0;
+    prices.forEach((price) => {
+      if (price.market_cap_usd > 50e6) {
+        data.push({
+          label: price.symbol,
+          value: price[sizeSelector],
+          link: `/prices/${price.symbol}`,
+          value2: colorAccessor(price),
+        });
+        if (colorAccessor(price) < min) min = colorAccessor(price);
+        if (colorAccessor(price) > max) max = colorAccessor(price);
+      }
+      totalCap += price.market_cap_usd;
+      vol24h += price.volume_usd_24h;
+    });
+    
+    const colors = (v) => {
+      if (v < 0) return d3.scale.linear().domain([-2, Math.log10(-min)]).range(['#000', 'red'])(Math.log(-v));
+      return d3.scale.linear().domain([-2, Math.log10(max)]).range(['000', 'green'])(Math.log(v));
+    };
+    return {
+      totalCap, vol24h, data, colors,
+    };
   }
 
   render() {
-    const { loading, data, colorAccessor, colors, totalCap, vol24h } = this.props;
+    const { loading } = this.props;
     return (!loading ? (
       <div className="Treemap">
         <div className="TreemapControls">
-            Total Market Cap: <b>${(totalCap / 1e9).toFixed(0)}B</b> Volume 24h: <b>${(vol24h / 1e9).toFixed(0)}B</b>
+            Total Market Cap: <b>${(this.state.totalCap / 1e9).toFixed(0)}B</b> Volume 24h: <b>${(this.state.vol24h / 1e9).toFixed(0)}B</b>
             &nbsp;Cell size:&nbsp;
             <DropdownButton id="sizeOptions" title={this.sizeMenu[this.state.radioSize]} value={this.state.radioSize} onSelect={this.setSize}>
               <MenuItem eventKey={1} padding={0}>{this.sizeMenu[0]}</MenuItem>
@@ -62,12 +108,12 @@ class Treemap extends React.Component {
           <TreeMap
             height={500}
             width={window.innerWidth - 80}
-            data={data}
+            data={this.state.data}
             valueUnit="USD"
             textColor="#ccc"
             fontSize={14}
-            colors={colors}
-            colorAccessor={colorAccessor}
+            colors={this.state.colors}
+            colorAccessor={p => p.value2}
           />
         </div>
       </div>
@@ -77,42 +123,12 @@ class Treemap extends React.Component {
 
 Treemap.propTypes = {
   loading: PropTypes.bool.isRequired,
-  data: PropTypes.array.isRequired,
-  colorAccessor: PropTypes.func.isRequired,
-  colors: PropTypes.func.isRequired,
+  prices: PropTypes.array.isRequired,
 };
 
 export default withTracker(() => {
-  const prices = LatestPrices.find().fetch();
-  const data = [];
-  let colorAccessor = p => p[colorSelector.get()];
-
-  let min = prices && prices[0] && colorAccessor(prices[0]);
-  let max = min;
-  let totalCap = 0;
-  let vol24h = 0;
-  prices.forEach((price) => {
-    if (price.market_cap_usd > 50000000) {
-      data.push({ label: price.symbol, value: price[sizeSelector.get()], link: `/prices/${price.symbol}`, value2: colorAccessor(price) });
-      if (colorAccessor(price) < min) min = colorAccessor(price);
-      if (colorAccessor(price) > max) max = colorAccessor(price);
-    }
-    totalCap += price.market_cap_usd;
-    vol24h += price.volume_usd_24h;
-  });
-
-  const colors = (v) => {
-    if (v < 0) return d3.scale.linear().domain([-2, Math.log10(-min)]).range(['#000', 'red'])(Math.log(-v));
-    return d3.scale.linear().domain([-2, Math.log10(max)]).range(['000', 'green'])(Math.log(v));
-  };
-  colorAccessor = p => p.value2;
-
   return {
     loading: false,
-    data,
-    colorAccessor,
-    colors,
-    totalCap,
-    vol24h,
+    prices: LatestPrices.find().fetch(),
   };
 })(Treemap);
