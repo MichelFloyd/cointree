cefa3e0621b59a60a92433cfb1903ebeca73021d
MichelFloyd <michel.floyd@cloaklabs.com>
2018-02-03 22:45:26 +0100

    Added K as a format

--- imports/modules/format-currency.js
+++ imports/modules/format-currency.js
@@ -1,6 +1,9 @@
+// format numbers as T (Trillion) B (Billion) M (Million) or K (thousands)
 const formatCurrency = (value) => {
-  if (value > 1e9) return `${Number.parseFloat(value / 1e9).toPrecision(3).toLocaleString()}B`;
-  return `${Number.parseFloat(value / 1e6).toPrecision(3).toLocaleString()}M`;
+  if (Math.abs(value) > 1e12) return `${Number.parseFloat(value / 1e12).toPrecision(3).toLocaleString()}T`;
+  else if (Math.abs(value) > 1e9) return `${Number.parseFloat(value / 1e9).toPrecision(3).toLocaleString()}B`;
+  else if (Math.abs(value) > 1e6) return `${Number.parseFloat(value / 1e6).toPrecision(3).toLocaleString()}M`;
+  return `${Number.parseFloat(value / 1e3).toPrecision(3).toLocaleString()}K`;
 };
 
 export default formatCurrency;
